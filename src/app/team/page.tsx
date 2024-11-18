5c3d8c14f3e134702c86b2e19b6830b9d06488a0
Oscar.Arias.Neto <kazinhoarias9@gmail.com>
2024-11-18 20:50:07 -0300

    Criando a pagina dos integrantes

---
+++ src/app/team/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Team: React.FC = () => (
+  <section>
+    <h1>Conheça nossa equipe</h1>
+    <ul>
+      <li>João Silva - Engenheiro</li>
+      <li>Maria Oliveira - Especialista em Vendas</li>
+      <li>Carlos Santos - Suporte Técnico</li>
+    </ul>
+  </section>
+);
+
+export default Team;
